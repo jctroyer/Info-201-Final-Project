2d99f9af9f8ffe050027ada0f8dead47c3ca8cb9
dorilusions <dorothyz.lu@gmail.com>
2019-11-19 06:04:42 -0800

    fleshed out the UI more

--- ui.R
+++ ui.R
@@ -1,15 +1,53 @@
+# Load packages
 library(shiny)
 
 # Define UI
 ui <- fluidPage(
+  
+  # Name of final project
   titlePanel("Global Terrorism"),
   tabsetPanel(
-    tabPanel("About The Project", fluid = TRUE),
     
+    # Project overview tab
+    tabPanel("About The Project",
+      fluid = TRUE,
+      h4("Background"),
+      p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+      h4("Research Question"),
+      p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+      h4("Conclusion"),
+      p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+
+      h4("About the Tech"),
+      p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    ),
+
     tabPanel("Data Visualization 1", fluid = TRUE),
-    
+
     tabPanel("Data Vizualization 2", fluid = TRUE),
-    
+
     tabPanel("About Us", fluid = TRUE)
   )
 )
