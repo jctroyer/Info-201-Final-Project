b2ac7a05de54f6120305c167b92d03f169a2f579
jctroyer <jjct@uw.edu>
2019-12-04 10:19:39 -0800

    Add images, edit text

--- ui.R
+++ ui.R
@@ -14,73 +14,66 @@ ui <- fluidPage(
   
   tabsetPanel(
     # Project overview tab
-    tabPanel("About The Project",
-      fluid = TRUE,
-
-      h3("ABOUT US"),
-      p("AUTHORS: Joseph Altamira, Jazmynn Combes-Troyer, Dorothy Lu"),
-      p("AFFILIATION:"),
-      p("Info-201: Technical Foundations of Informatics"),
-      p("The Information School"),
-      p("University of Washington"),
-      p("Autumn 2019"),
+    tabPanel("Project Overview", fluid = TRUE, sidebarLayout(
+      sidebarPanel(
+        h3("BACKGROUND | What is Terrorism?"),
+        p("Terrorism is the unlawful and systematic use of violence against a state,
+        country, or group of people, motivated by various ideologies, as a form of
+        political coercion. Global acts of terrorism have impacted hundreds, thousands,
+        and millions of people throughout history in social, economic, and political ways."),
+        p("Terrorism occurs on local, national, and international levels.
+          Take - for instance - the white supremacist attack at the Unite the Rally
+          attack in 2019, which occurred on US soil in Charlottesville, VA by Americans.
+          This act of terrorism occurred on a more local level. Another example is the 
+          infamous al-Qaeda attack on the USA on September 11, 2001, which was a national
+          terrorist attack. On an international level, one can look back on World War II
+          and the Holocaust, which mainly targeted Jewish populations in Western Europe."),
+        p("Global terrorism is a threat to the health and well-being of both the 
+          targets at which the attacks are aimed, and to the innocent people that
+          sustain loss and damage as a perhaps unintended result of those attacks."),
+        h3("OUR PROJECT | Why do we Care?"),
+        p("Terrorism affects individuals, communities, and societies on multiple levels.
+          Aside from the obvious physical and psychological impacts on direct victims
+          of terrorism, terrorism causes deep-rooted fear and insecurity within affected
+          populations. This problem is pertinent for any civilian or member of society as
+          it is difficult to predict when or where the next attack will occur. Therefore,
+          it is important to be aware of the ways in which one can be affected and to
+          potentially prepare ways in which to respond. It is also important in order 
+          to be an informed citizen and to be aware of global issues."),
+        p("The sensitive subject of terrorist attacks will be addressed by 
+          bringing attention to the number of terrorist attacks that have occurred,
+          where they occurred, and who and what was affected. By gaining awareness 
+          of these aspects of terrorism and finding correlations between various data
+          about those attacks (date, location, cause, etc.), we can raise awareness 
+          and educate the public about terrorism and about targeted regions that are 
+          not widely publicized by the media."),
+        p("We seek to shed light on global terrorist attacks, particularly on regions of
+          the world that people would not otherwise consider. Another goal is to put
+          the effects of terrorism into perspective with the number of individuals affected
+          as a result of these attacks, whether it was intentional or unintentional. 
+          Anyone could be a victim and anyone can be affected at any given time."),
+        h3("THE DATA"),
+        p("The dataset that will be used throughout this project is called 
+          Global Terrorism Database from kaggle.com. It lists over 180,000
+          terrorist attacks from 1970 to 2017. The GTD contains more than 100 variables,
+          using information from media outlets/articles that are not classified.
+          This project focuses on data from 2000-2017.")
+      ),
       
-      h3("BACKGROUND"),
-      p("Terrorism is the unlawful and systematic use of violence against a
-        state, country, or group of people, motivated by various ideologies,
-        as a form of political coercion. Global acts of terrorism have impacted
-        hundreds, thousands, and millions of people throughout history in
-        social, economic, and political ways."),
-
-      h3("STAKEHOLDERS"),
-      p("Direct stakeholders include political entities (i.e. a government,
-        political figure, or organization), both as targets and instigators.
-        As instigators, political entities plan and execute attacks against
-        other states, nations, other political groups, or specific political
-        figures to further some political, religious, or other ideological
-        objective. As targets, these political entities suffer damage from
-        their attackers which may include death, injury, and political and
-        economic harm."),
-      p("Indirect stakeholders include civilians residing in or around the
-        targeted person, group, or nation. These civilians are typically
-        citizens of a state and are not direct or intended targets. Civilians
-        may suffer personal loss via death or injury of themselves or loved
-        ones as well as property damage or socioeconomic difficulties that
-        may result from damage sustained from main stakeholders (such as
-        governments or major political leaders)."),
-
-      h3("WHY STUDY GLOBAL TERRORISM?"),
-      p("The members of our group, like many Americans and other citizens
-        around the world, is generally uninformed about both small and large
-        scale terrorist attacks that have happened in the last few decades.
-        We are interested in this topic because we would like to learn more
-        on how these attacks could potentially affect us if we as a community,
-        state, or nation were to become the next target of an attack. In other
-        words, what are the effects of terrorism on individuals and their
-        societies?"),
-
-      h3("ABOUT THE DATA"),
-      p("The dataset that is being used throughout this project is called the
-        Global Terrorism Database from kaggle.com.  The database is maintained
-        by researchers at the National Consortium for the Study of Terrorism and
-        Responses to Terrorism (START), headquartered at the University of
-        Maryland. All data wrangling was done in R and deployed on this site
-        using Shiny.  Additionally, we used packages such as ggplot and leaflet
-        to create the engaging data visualizations."),
-      
-      h3("MORE INFO"),
-      p("For more detailed information about this project, feel free to view the
-        project's technical report, which can be found ",
-        a("here", href="https://github.com/jctroyer/Info-201-Final-Project/wiki",
-          target="_blank"))
+      mainPanel(
+        img(src = "https://www.gcsp.ch/sites/default/files/2019-04/Geneva%20Launch%20of%20Global%20Terrorism%20Index%20Report%202018_Web.jpg")
+      )
+    )
     ),
 
     tabPanel("Casualties Map", fluid = TRUE,sidebarLayout(
       sidebarPanel(
         h4("Casualties By Attack Type"),
-        p("Choose an attack type to see how much a country has been affected by
-          that specific type of attack.  Click on the map to view the more detailed
-          casualty statistics."),
+        p("Select an attack type to see how many individuals were affected
+          (by personal death or injury) as a result of that type of attack.
+          Zoom in on the map to view countries and cities and click on a point
+          of attack to view more detailed casualty statistics such as the number
+          of people killed and wounded by that attack."),
         selectizeInput("attack_type", "Attack Type", attack_target,
           multiple = FALSE,
           options = list(
@@ -99,7 +92,10 @@ ui <- fluidPage(
       sidebarLayout(
         sidebarPanel(
           h4("Number of Individuals Affected by Attack Type by Country"),
-          p("Choose two countries to compare number of individuals affected."),
+          p("Select two countries via the drop down menu
+            (or search and select a country by typing the country's name)
+            to compare the number of individuals affected by the attack types
+            used in the chosen countries. "),
           selectizeInput(inputId = "country_one", label = "Country 1",
                          choices = country, multiple = FALSE, 
                          options = list(placeholder = "Select Country or Type to Search")),
@@ -117,7 +113,9 @@ ui <- fluidPage(
     tabPanel("Summary Table", fluid = TRUE, sidebarLayout(
       sidebarPanel(
         h4("Summary Table"),
-        p("This table will display specific information about different regions. Please select a region:"),
+        p("Select a region of the world (or search and select by typing the name of a region)
+          to view a table that displays summary statistics about
+          terrorist attacks in the chosen region. Search for certain stats using the search bar."),
         selectizeInput("region", "Region", region,
                        multiple = FALSE,
                        options = list(
@@ -132,18 +130,29 @@ ui <- fluidPage(
     )
     ),
 
-    tabPanel("Conclusion",
-      fluid = TRUE,
-      h3("CONCLUSION"),
-      p("We learned that global terrorism is immensely complex and that it is
-        much too broad, complex, and large of an issue to make causal or
-        predictive claims and analyses. However, through discussion about
-        research questions, data visualizations, and future plans, we have
-        learned to develop our critical thinking and coding skills. Another
-        noteworthy lesson we learned was that it can be difficult to find
-        datasets to fit our needs, and sometimes, we need to challenge ourselves
-        to work with what we have available and to generate creative
-        solutions.")
+    tabPanel("Conclusion", fluid = TRUE, sidebarLayout(
+      sidebarPanel(
+        h3("CONCLUSION"),
+        p("According to the insights gathered from the visualizations,
+          it seems that a majority of terrorist attacks that occurred in 
+          the 21st century were targeted at countries in Africa and the Middle East.
+          That is not too shocking, given the political unrest and wars occurring
+          in those regions of the world."),
+        p("We learned that global terrorism is much too broad, complex,
+        and large of an issue to make causal or predictive claims and analyses.
+        However, through discussion about research questions, data visualizations,
+        and future plans, we have learned to develop our critical thinking
+        and coding skills. Another noteworthy lesson we learned was that it
+        can be difficult to find datasets to fit our needs, and sometimes,
+        we need to challenge ourselves to work with what we have available
+        and to generate creative solutions.
+")
+      ),
+      
+      mainPanel(
+        img(src = "https://communicationinprint.files.wordpress.com/2016/07/cartoon-2.jpg?w=1300&h=768&crop=1")
+      )
+    )
     )
   )
 )
