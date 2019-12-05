bd93c6660f6e90f0fec0e6a3ed5f9f6fc20a7ff4
dorilusions <dorothyz.lu@gmail.com>
2019-12-04 19:12:59 -0800

    revised Unknown var

--- data/terror_data.R
+++ data/terror_data.R
@@ -15,11 +15,13 @@ world <- readOGR("data/world_shape_file/TM_WORLD_BORDERS_SIMPL-0.3.shp")
 terrorism_select <- terrorism %>%
   dplyr::select(country_txt, attacktype1_txt, nkill, nwound, latitude, longitude)
 
+terrorism_select$attacktype1_txt[terrorism_select$attacktype1_txt == "Unknown"] <- "Unspecified Attack Type"
+
 # Merge data
 # merged_data <- sp::merge(world, terrorism_unique, by.x = "NAME", by.y = "country_txt", duplicateGeoms = TRUE, all.x = FALSE)
 
 # Get list of types of attack targets
-attack_target <- terrorism %>%
+attack_target <- terrorism_select %>%
   dplyr::select(attacktype1_txt) %>%
   unique()
 
